35896ee9d6354e9dc0f7cb45319ae90eccffc814
tomi34 <66636695+tomi34@users.noreply.github.com>
2021-04-03 17:01:38 +0200

    main.blocks, main.ts frissítése

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+let ertekem = 0
 input.onButtonPressed(Button.A, function () {
+    ertekem = pins.analogReadPin(AnalogPin.P1)
+    basic.showString("" + (ertekem - 50) + "-" + (ertekem + 50))
+})
+input.onPinPressed(TouchPin.P2, function () {
 	
 })
